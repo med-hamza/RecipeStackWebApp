040274ba42fa9f0573b2d3e6080ce1ad459b7dfe
med-hamza <mohamedehamza32@gmail.com>
2024-04-03 23:00:13 +0100

    maodif page animation load

--- front-end/src/components/Progressbar.jsx
+++ front-end/src/components/Progressbar.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-import Lottie from 'react-lottie';
-import animationData from '../assest/images/loading-food.json'
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+import animationData from '../assest/images/loading-food.json';
 
 const Progressbar = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-  };
+  const animationContainer = useRef(null);
 
-  return <div className=" w-28 h-28"> <Lottie options={defaultOptions} /></div>;
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: animationContainer.current, // the container element
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: animationData, // the animation data
+    });
+
+    return () => anim.destroy(); // Clean up function to stop and dispose of the animation instance
+  }, []); // Empty dependency array means this effect runs only once after the initial render
+
+  return <div className="w-28 h-28" ref={animationContainer}></div>;
 };
 
 export default Progressbar;
