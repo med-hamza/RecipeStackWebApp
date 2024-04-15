2219b05963771fbb161008d752fe13d362a4d586
med-hamza <mohamedehamza32@gmail.com>
2024-04-15 13:39:53 +0100

    modif section FeatureRecipe

--- front-end/src/Layout/Footer.jsx
+++ front-end/src/Layout/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
       <div className="  bg-[#000c] copyright border-t-[1px] py-3 px-10 border-dashed border-white">
         <div className="flex sm:flex-row flex-col justify-between items-center">
           <div><p className="text-white text-base sm:text-left text-center sm:mb-0 mb-5">© 2023 Mohamed Hamza Trabelsi. All rights Reserved</p></div>
-          <div className="flex items-center"><Link className="sm:text-right text-center" target="blank" to="https://www.linkedin.com/in/mohamed-hamza-trabelsi"><img class="mr-2" src={process.env.PUBLIC_URL + '/icones/linkedin.svg'} alt="linkedin" /></Link>
+          <div className="flex items-center"><Link className="sm:text-right text-center" target="blank" to="https://www.linkedin.com/in/mohamed-hamza-trabelsi"><img className="mr-2" src={process.env.PUBLIC_URL + '/icones/linkedin.svg'} alt="linkedin" /></Link>
             <Link to="https://www.instagram.com/t_m_hamza/" target="blank"><img src={process.env.PUBLIC_URL + '/icones/instagram.svg'} alt="instgram" /></Link>
           </div></div></div>
     </div>
