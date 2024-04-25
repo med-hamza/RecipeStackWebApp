7dc1220c2ff858f8cff246a40daa9dc96ffac453
med-hamza <mohamedehamza32@gmail.com>
2024-04-25 12:03:10 +0100

    change baseUrl

--- front-end/src/reducers/slices/apiSlice.js
+++ front-end/src/reducers/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL });
 export const apiSlice = createApi({
     baseQuery,
     tagTypes: ['User'],
