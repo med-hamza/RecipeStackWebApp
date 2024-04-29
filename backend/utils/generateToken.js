17d03e8ef69f4638ce9149d0ab45fafa1f84f479
med-hamza <mohamedehamza32@gmail.com>
2024-04-29 11:36:48 +0100

    modif in token file

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const gernerateToken = (res, userId) => {
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
+        secure: true,
         sameSite: 'strict',
         maxAge: 30 * 24 * 60 * 1000
 
