360e175bfc7f8c2c7d0791c748db894e4edda5e3
med-hamza <mohamedehamza32@gmail.com>
2024-05-01 14:40:12 +0100

    modif in user Api slice

--- front-end/src/reducers/slices/usersApiSlice.js
+++ front-end/src/reducers/slices/usersApiSlice.js
@@ -1,6 +1,6 @@
 import { apiSlice } from "./apiSlice";
 const USER_URL = '/users';
-const BASE_URL_SIGNUP_SIGNIN = process.env.NODE_ENV === "development" ? "http://localhost:3000" : 'http://localhost:3000'
+const BASE_URL_SIGNUP_SIGNIN = process.env.NODE_ENV === "development" ? "http://localhost:3000" : 'https://recipe-web-app-seven.vercel.app/'
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
